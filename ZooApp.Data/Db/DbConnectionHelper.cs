f456405ca2b8b6df1cddd0562b593f51594f9f18
Paw Møller <pawmoeller94@gmail.com>
2026-04-29 17:33:45 +0200

    Add ZealandZoo web project and static assets
    Add a new ZealandZoo Razor Pages web project including Program.cs, ZealandZoo.csproj, Pages (Index, Privacy, Error, shared _Layout), appsettings, and wwwroot static assets (Bootstrap, jQuery, site.css/js). Include .gitattributes and various VS/obj build and metadata files. Also modify ZooApp.Data: update Db/DbConnectionHelper.cs and database_generator.txt; update IDE metadata and cache files.

--- ZooApp.Data/Db/DbConnectionHelper.cs
+++ ZooApp.Data/Db/DbConnectionHelper.cs
@@ -18,8 +18,8 @@ namespace ZooApp.Data.Db
         { 
             var builder = new SqlConnectionStringBuilder
             {
-                DataSource = @"SHARK1-PC\SQLEXPRESS",
-                InitialCatalog = "dev_ZealandZoo_0_1",
+                DataSource = @"PAX-LAPTOP\SQLEXPRESS",
+                InitialCatalog = "dev_ZealandZoo_0_3",
                 TrustServerCertificate = true,
                 IntegratedSecurity = true // For Windows Auth
             };
--- ZooApp.Services/obj/Debug/net9.0/ZooApp.Services.AssemblyInfo.cs
+++ ZooApp.Services/obj/Debug/net9.0/ZooApp.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZooApp.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10ee9832ed75b59216b75db7dff71c14af193c70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c00eda510b2f0c86e394700f83816ba9cfacf822")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZooApp.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZooApp.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
