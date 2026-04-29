423c1b5990b094170e0e7b9a1c845168c3d0373b
nikolairasmusson-lgtm <nikolai.rasmusson@gmail.com>
2026-04-29 12:59:05 +0200

    Hash guest passwords in GuestService
    Add PasswordHasher<string> to GuestService and hash guest.Password before calling MockGuests.AddGuest. Also added Microsoft.AspNetCore.Identity using and fixed class/method structure (removed previously nested method declarations and organized GetGuests/AddGuest as proper instance methods).

--- Services/GuestService.cs
+++ Services/GuestService.cs
@@ -1,20 +1,26 @@
-﻿using ZealandZoo.MockData;
+﻿using Microsoft.AspNetCore.Identity;
+using ZealandZoo.MockData;
 using ZealandZoo.Models;
 
 namespace ZealandZoo.Services
 {
-    public class GuestService
-        {
-            public List<Guest> GetGuests()
-            {
-                return MockGuests.GetMockGuests();
-            }
 
-            public void AddGuest(Guest guest)
-            {
-                MockGuests.AddGuest(guest);
-            }
+    public class GuestService
+    {
+        private PasswordHasher<string> _passwordHasher = new PasswordHasher<string>();
+
+        public List<Guest> GetGuests()
+        {
+            return MockGuests.GetMockGuests();
         }
+
+        public void AddGuest(Guest guest)
+        {
+            guest.Password = _passwordHasher.HashPassword(null, guest.Password);
+
+            MockGuests.AddGuest(guest);
+        }
+    }
 }
 
 
