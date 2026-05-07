eafad1ae8c76d6db0a8ab587d7be7843ec617dd9
Lucascheetah <Lucassmedengaard@gmail.com>
2026-05-07 09:10:39 +0200

    Alt backend uden ui

---
+++ ZooApp.Data/interfaces/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooApp.Domain.Models;
+
+namespace ZooApp.Data.interfaces
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+
+        public User Create(User entity);
+        public User GetById(int id);
+        public IEnumerable<User> GetAll();
+        public User Update(User entity);
+        public User Delete(int id);
+    }
+}
+
---
+++ ZooApp.Domain/Models/InventoryItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooApp.Domain.Models
+{
+    public class InventoryItem
+    {
+        public int Id { get; set; }
+        public int Category { get; set; }
+        public string? Name { get; set; }
+        public int Quantity { get; set; }
+
+        
+        public InventoryItem(int id, int category,string name, int quantity)
+        {
+            Id = id;
+            Category = category;
+            Name = name;
+            Quantity = quantity;
+        }
+
+
+      
+    }
+}
