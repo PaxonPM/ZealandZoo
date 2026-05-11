cd295aa818ae9992200e41dde48ca38ce75d1df4
Paw Møller <pawmoeller94@gmail.com>
2026-05-11 12:53:59 +0200

    Update DB connection and generator DB name
    Change SQL connection DataSource from PAX-LAPTOP\SQLEXPRESS to SHARK1-PC\SQLEXPRESS (previous value left commented) in DbConnectionHelper.cs. Also update database_generator.txt to use dev_ZealandZoo_0_4 instead of dev_ZealandZoo_0_3 so the script targets the newly created database.

--- ZooApp.Data/Db/DbConnectionHelper.cs
+++ ZooApp.Data/Db/DbConnectionHelper.cs
@@ -18,7 +18,7 @@ namespace ZooApp.Data.Db
         { 
             var builder = new SqlConnectionStringBuilder
             {
-                DataSource = @"PAX-LAPTOP\SQLEXPRESS",
+                DataSource = @"SHARK1-PC\SQLEXPRESS",//@"PAX-LAPTOP\SQLEXPRESS",
                 InitialCatalog = "dev_ZealandZoo_0_4",
                 TrustServerCertificate = true,
                 IntegratedSecurity = true // For Windows Auth
