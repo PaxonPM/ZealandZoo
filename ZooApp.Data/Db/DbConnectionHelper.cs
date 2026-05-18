0945adbd840eedf18c4ea5318847c8d4c35c4b41
Paw Møller <pawmoeller94@gmail.com>
2026-05-18 12:23:27 +0200

    Update DB InitialCatalog to dev_ZealandZoo_0_8
    Change the SqlConnectionStringBuilder InitialCatalog in DbConnectionHelper.cs from dev_ZealandZoo_0_4 to dev_ZealandZoo_0_8 so the development connection string points to the updated dev database.

--- ZooApp.Data/Db/DbConnectionHelper.cs
+++ ZooApp.Data/Db/DbConnectionHelper.cs
@@ -19,7 +19,7 @@ namespace ZooApp.Data.Db
             var builder = new SqlConnectionStringBuilder
             {
                 DataSource = @"SHARK1-PC\SQLEXPRESS",//@"PAX-LAPTOP\SQLEXPRESS",
-                InitialCatalog = "dev_ZealandZoo_0_4",
+                InitialCatalog = "dev_ZealandZoo_0_8",
                 TrustServerCertificate = true,
                 IntegratedSecurity = true // For Windows Auth
             };
