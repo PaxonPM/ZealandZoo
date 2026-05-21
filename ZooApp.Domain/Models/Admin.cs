aaaf4ce1d863925e24532221ebf0ed9b79ebc623
Paw Møller <pawmoeller94@gmail.com>
2026-05-21 17:53:37 +0200

    Add UserModel; remove legacy models
    Removed legacy model classes (Admin.cs, GuestModel.cs, Person.cs, User.cs) and added a new UserModel.cs that includes data annotations, Danish validation messages, an @edu.zealand.dk email regex, and constructors. Updated Event.cs by cleaning unused usings, translating validation error messages to Danish, adding [Required] for MaxParticipants and making it a nullable int with an adjusted Range message. These changes consolidate user-related models and localize validation text.

--- ZooApp.Domain/Models/Admin.cs
+++
@@ -1,9 +0,0 @@
-namespace ZooApp.Domain.Models
-{
-    public class Admin
-    {
-        public int Id { get; set; }
-        public required string Username { get; set; }
-        public required string Password { get; set; }
-    }
-}
---
+++ ZooApp.Domain/Models/UserModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using ZooApp.Domain.Models;
+
+namespace ZooApp.Domain.Models
+{
+    public class UserModel
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Navn er påkrævet")]
+        [StringLength(50, ErrorMessage = "navn skal være under 50 tegn")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Email er påkrævet")]
+        [RegularExpression(@"^[^@\s]+@edu\.zealand\.dk$", ErrorMessage = "Email skal være en @edu.zealand.dk adresse")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Telefonnummer er påkrævet")]
+        [StringLength(8, MinimumLength = 8, ErrorMessage = "Telefonnummer skal være præcis 8 tegn")]
+        public string Telefon { get; set; }
+
+        [Required(ErrorMessage = "kodeord er påkrævet")]
+        public string PwHash { get; set; }
+        public int RoleId { get; set; }
+        public bool IsNotificationActive { get; set; }
+
+        public UserModel() { }
+
+        public UserModel(string name, string email, string telefon, string pwHash, int roleId, bool isNotificationActive)
+        {
+            
+            Name = name;
+            Email = email;
+            Telefon = telefon;
+            PwHash = pwHash;
+            RoleId = roleId;
+            IsNotificationActive = isNotificationActive;
+        }
+    }
+}
